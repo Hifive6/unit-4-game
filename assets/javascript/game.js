6ce1cf1646e6a767702bb0e0007b0b1f15eb5aa3
Hi56 <39879994+Hifive6@users.noreply.github.com>
2018-07-23 18:06:53 -0400

    added restart function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,13 +5,12 @@
 
 $(document).ready(function(){
 
-    //These are our chosen functions for the game//
+    //These are our chosen variables for the game//
         var wins = 0;
         var loss = 0;
         var userScore = 0;
         var genRandom = 0;
-        // var genbtn = " ";
-        var picChosen = 0;
+        var num = 0;
     
         
     //Made this function so that I could call back later for the rules of winnings
@@ -19,30 +18,31 @@ $(document).ready(function(){
             wins += 1;
             alert("Winner Winner");
             $("#win").html(wins + " <br>");
+            $("#picture").empty();
             num = 0;
             startReset();
         }
+
     //this function is calling back later and used for the rules of losing
         function Lose(){
-            loss += 1;
+            loss += 1
             alert("Too Bad");
             $("#lose").html(loss);
+            $("#picture").empty();
             num = 0;
             startReset();
         }
     //next on the document when the document loads I need the generated number to be created then stored.
-    // function startReset(){
+    function startReset(){
     
-    // $("#picture").empty();
+    
+    
     //This is were the generated number is from btw 19 -  120
-    var genRandom = Math.floor(Math.random() * 101) + 19;
+    genRandom = Math.floor(Math.random() * 101) + 19;
     
-    
-    //This is were we will put the generated number  and the var equal to a string so we can call genNumber back into the string to place it
-    // genNumber = random + genNumber;
      console.log(genRandom);
     
-    //this is function will not add the value that has been stored in genNumber and add to the div on the html behind the random number words in that div.
+    //this is function will add the value that has been stored in genNumber and add to the div on the html behind the random number words in that div.
     $("#genNumber").html("<br><div>" + genRandom);
     
     
@@ -56,27 +56,29 @@ $(document).ready(function(){
         picture.addClass("btn btn-outline-primary number");
         picture.attr("data-random", ranBtn);
         
-        
-         
+        // $("#picture").empty();
+    //     
         $("#picture").append("<div><br>", picture);
-          
+          console.log($("#picture"));
         }
     
-    
-    // startReset();
+    }
+    startReset();
     
     //This will create an onclick event attacthed the button I created above
     $(".number").on("click",function() {
     //    need to add parseIntin here
-        var userScore = parseInt($(this).attr("data-random"));
+        userScore = parseInt($(this).attr("data-random"));
         
         
-        var num = picChosen += userScore;
+        num = num + userScore;
         console.log(num);
         $("#score").html(num);
     
         //this is my if else statement for deciding how the game will decide wins and losess
         if (num > genRandom){
+            // console.log(num);
+            // console.log(genRandom);
             Lose();
          }
     
